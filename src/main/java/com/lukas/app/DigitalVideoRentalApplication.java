7970edd10b2f205000b754235470c724c4a227ac
L0REC <lukas2500@gmail.com>
2025-07-25 14:39:08 +0900

    Initial commit

---
+++ src/main/java/com/lukas/app/DigitalVideoRentalApplication.java
@@ -0,0 +1,13 @@
+package com.lukas.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalVideoRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalVideoRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lukas/app/DigitalVideoRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukas.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalVideoRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
