00ebabe81edcc7cf8242e6700745dd596e6f9fa2
L0REC <lukas2500@gmail.com>
2025-07-31 12:17:14 +0900

    videosテーブルのマッピング追加

---
+++ src/main/java/com/lukas/app/mapper/VideoMapper.java
@@ -0,0 +1,14 @@
+package com.lukas.app.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.lukas.app.domain.Video;
+
+@Mapper
+public interface VideoMapper {
+
+	List<Video> selectVideos();
+	
+}
