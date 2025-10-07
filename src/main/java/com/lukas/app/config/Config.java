ff153ca308b7a28d4b56395dc02da57b244a48cf
L0REC <lukas2500@gmail.com>
2025-10-07 12:19:04 +0900

    フィルターをConfigに移動

--- src/main/java/com/lukas/app/config/Config.java
+++ src/main/java/com/lukas/app/config/Config.java
@@ -1,26 +1,54 @@
 package com.lukas.app.config;
 
+import jakarta.servlet.Filter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.lukas.app.filter.AuthFilter;
-import com.lukas.app.filter.LoggingFilter;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Configuration
 public class Config {
 
 	@Bean
-	FilterRegistrationBean<AuthFilter> authFilter() {
-		var filter = new FilterRegistrationBean<>(new AuthFilter());
+	FilterRegistrationBean<Filter> authFilter() {
+		var filter = new FilterRegistrationBean<Filter>((request, response, chain) -> {
+		var req = (HttpServletRequest) request;
+		var res = (HttpServletResponse) response;
+		
+		if(req.getSession().getAttribute("user")==null) {
+			log.warn("不正なアクセス");
+			res.sendRedirect("/login");
+			return;
+		}
+		
+		chain.doFilter(request, response);
+		
+		});
 		filter.addUrlPatterns("/dashboard/*", "/catalog/*");
 		filter.setOrder(2);
 		return filter;
 	}
 	
 	@Bean
-	FilterRegistrationBean<LoggingFilter> loggingFilter() {
-		var filter = new FilterRegistrationBean<>(new LoggingFilter());
+	FilterRegistrationBean<Filter> loggingFilter() {
+		var filter = new FilterRegistrationBean<Filter>((request, response, chain) -> {
+			var req = (HttpServletRequest) request;
+			String uri = req.getRequestURI();
+			
+			if(!uri.endsWith(".css")&&!uri.endsWith(".js") && !uri.endsWith(".ico") 
+	                && !uri.endsWith(".png") && !uri.endsWith(".jpg") && !uri.endsWith(".gif")) {
+				log.info("{}:{}", req.getMethod(), uri);
+			}
+			
+			chain.doFilter(request, response);
+			
+		});
+		
 		filter.addUrlPatterns("/*");
 		filter.setOrder(1);
 		return filter;
